9c0ce087bc08ae702fea980e244118ab9e197ade
darzokako-maker <darzokako@gmail.com>
2026-06-13 21:23:39 +0300

    Add null checks for matrix and boneMatrix

--- Calculate.cs
+++ Calculate.cs
@@ -9,6 +9,8 @@ namespace CS2_ESP
     {
         public static Vector2 WorldToScreen(float[] matrix, Vector3 pos, Vector2 windowSize)
         {
+            if (matrix == null || matrix.Length < 16) return new Vector2(-99, -99);
+
             float screenW = (matrix[12] * pos.X) + (matrix[13] * pos.Y) + (matrix[14] * pos.Z) + matrix[15];
 
             if (screenW > 0.001f)
@@ -27,14 +29,22 @@ namespace CS2_ESP
         public static List<Vector3> ReadBones(IntPtr boneMatrix, Swed swed)
         {
             List<Vector3> bones = new List<Vector3>();
+            if (boneMatrix == IntPtr.Zero || swed == null) return bones;
 
-            foreach (BoneIds boneId in Enum.GetValues(typeof(BoneIds)))
+            try
             {
-                float x = swed.ReadFloat(boneMatrix + ((int)boneId * 32) + 0);
-                float y = swed.ReadFloat(boneMatrix + ((int)boneId * 32) + 4);
-                float z = swed.ReadFloat(boneMatrix + ((int)boneId * 32) + 8);
+                foreach (BoneIds boneId in Enum.GetValues(typeof(BoneIds)))
+                {
+                    float x = swed.ReadFloat(boneMatrix + ((int)boneId * 32) + 0);
+                    float y = swed.ReadFloat(boneMatrix + ((int)boneId * 32) + 4);
+                    float z = swed.ReadFloat(boneMatrix + ((int)boneId * 32) + 8);
 
-                bones.Add(new Vector3(x, y, z));
+                    bones.Add(new Vector3(x, y, z));
+                }
+            }
+            catch
+            {
+                bones.Clear();
             }
 
             return bones;
@@ -43,6 +53,7 @@ namespace CS2_ESP
         public static List<Vector2> ReadBones2D(List<Vector3> bones3D, float[] viewMatrix, Vector2 screenSize)
         {
             List<Vector2> bones2D = new List<Vector2>();
+            if (bones3D == null || bones3D.Count == 0 || viewMatrix == null) return bones2D;
 
             foreach (var bone in bones3D)
             {
